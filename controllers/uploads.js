a4cc7f004d08f8bda1b1282b9eff27ff8aa83dbe
Zully Chumpitaz Castillo <zully.chumpitaz@gmail.com>
2022-04-24 21:34:52 -0500

    Upload to Cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const fs = require('fs');
+
 const { response } = require("express");
+const cloudinary = require('cloudinary').v2;
+cloudinary.config( process.env.CLOUDINARY_URL );
+
 const { uploadFile } = require("../helpers");
 const { User, Product } = require("../models");
 
@@ -100,8 +104,56 @@ const showImage = async (req, res = response) => {
     const defaultImage = path.join( __dirname, '../assets/no-image.jpg');
     res.sendFile( defaultImage );
 }
+
+const updateImageCloudinary = async ( req, res = response) => {
+    const { id, collection } = req.params;
+
+    let modelo;
+
+    switch ( collection ) {
+        case 'users':
+            modelo = await User.findById( id );
+            if( !modelo ){
+                return res.status(400).json({
+                    msg: `No existe un usuario con el ID: ${ id }`
+                });
+            }
+            break;
+        case 'products':
+                modelo = await Product.findById( id );
+                if( !modelo ){
+                    return res.status(400).json({
+                        msg: `No existe un producto con el ID: ${ id }`
+                    });
+                }
+                break;
+        default:
+            res.status(500).json({ msg: 'Se me olvidó validar esto'});
+    }
+
+    // Limpiar imágenes previas de Cloudinary
+    if( modelo.img ){
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[ nombreArr.length - 1 ];
+        const [ public_id ] = nombre.split('.');
+
+        cloudinary.uploader.destroy( public_id );
+    }
+
+    const { tempFilePath } =  req.files.file;
+    const { secure_url } = await cloudinary.uploader.upload( tempFilePath );
+
+    modelo.img = secure_url;
+    await modelo.save();
+
+    res.json({
+        modelo
+    });
+}
+
 module.exports = {
     upload,
     updateImage,
-    showImage
+    showImage,
+    updateImageCloudinary
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { upload, updateImage, showImage } = require('../controllers/uploads');
+const { upload, updateImageCloudinary, showImage } = require('../controllers/uploads');
 const { validCollections } = require('../helpers');
 const { validateJWT, validateFields, validateFile } = require('../middlewares');
 
@@ -20,7 +20,7 @@ router.put('/:collection/:id',
         check('id', 'El id debe ser un ID de Mongo').isMongoId(),
         check('collection').custom( c => validCollections( c, ['users', 'products'])),
         validateFields
-    ], updateImage);
+    ], updateImageCloudinary);
 
 router.get('/:collection/:id',
     [
